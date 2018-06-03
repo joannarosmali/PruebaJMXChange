457a6c5a1cea41678f9479824da635ee69e4fdbe
Joanna <Joanna@Joanna-PC>
2018-06-03 15:11:42 -0400

    Ajustes en el pom para incluir las referencias Adecuadas y en la estructura de la clase

--- src/main/java/com/test/xchange/PruebaJMXChange.java
+++ src/main/java/com/test/xchange/PruebaJMXChange.java
@@ -1,10 +1,9 @@
 
 package com.test.xchange;
 
-import java.io.IOException;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.binance.BinanceExchange;
 import org.knowm.xchange.bitstamp.BitstampExchange;
 import org.knowm.xchange.currency.CurrencyPair;
@@ -21,29 +20,29 @@ public class PruebaJMXChange {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		Exchange bitstamp = ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class.getName());
-		//Exchange bitstamp = ExchangeFactory.INSTANCE.createExchange(BinanceExchange.class.getName());
 
-		Exchange binancestamp = ExchangeFactory.INSTANCE.createExchange(new BinanceExchange().getDefaultExchangeSpecification());
-		//MarketDataService marketDataService = bitstamp.getMarketDataService();
+		//Exchange binancestamp = ExchangeFactory.INSTANCE.createExchange(BinanceExchange.class.getName());
+		ExchangeSpecification exSpec = new BinanceExchange().getDefaultExchangeSpecification();
+		exSpec.setExchangeSpecificParametersItem("recvWindow", 10000L);
+		Exchange binancestamp = ExchangeFactory.INSTANCE.createExchange(exSpec);
 
-		//Ticker ticker;
-		try {
-			//ticker = marketDataService.getTicker(CurrencyPair.BTC_USD);
-			System.out.println(getTicker(bitstamp.getMarketDataService(), CurrencyPair.BTC_USD).toString());
-			System.out.println(getTicker(binancestamp.getMarketDataService(), CurrencyPair.BTC_USD).toString());
-			
-			/*BitstampTicker bitstampTicker = ((BitstampMarketDataServiceRaw)marketDataService).getBitstampTicker(CurrencyPair.BTC_USD);
-			System.out.println(bitstampTicker.toString());*/
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		//ticker = marketDataService.getTicker(CurrencyPair.BTC_USD);
+		System.out.println("Bitstamp --> "+getTicker(bitstamp.getMarketDataService(), CurrencyPair.BTC_USD).toString());
+		
+		System.out.println("Binance  --> "+getTicker(binancestamp.getMarketDataService(), CurrencyPair.BTC_USD));
+
 	}
-	
-	private static Ticker getTicker (MarketDataService marketDataService, CurrencyPair currencyPair) throws IOException{
-		return marketDataService.getTicker(currencyPair);
+
+	private static Ticker getTicker (MarketDataService marketDataService, CurrencyPair currencyPair)/* throws IOException*/{
+		try {
+			return marketDataService.getTicker(currencyPair);
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+			return null;
+		}
 	}
 
 }
