39a7942ace07d23d2ed33f43f70c40faf79166d6
Joanna Mangarre <joanna@hazling.com>
2018-06-04 09:30:43 -0400

    Ajuste en los imports

--- src/main/java/com/test/xchange/PruebaJMXChange.java
+++ src/main/java/com/test/xchange/PruebaJMXChange.java
@@ -1,20 +1,12 @@
 
 package com.test.xchange;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.binance.BinanceExchange;
-import org.knowm.xchange.binance.service.BinanceMarketDataService;
 import org.knowm.xchange.bitstamp.BitstampExchange;
-import org.knowm.xchange.currency.Currency;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Ticker;
-import org.knowm.xchange.dto.meta.CurrencyMetaData;
-import org.knowm.xchange.dto.meta.CurrencyPairMetaData;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 
 /**
